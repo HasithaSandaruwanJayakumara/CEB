8255e9da864d57334e8db6c775f8b4e832226f8e
HasithaSandaruwanJayakumara <texthasitha@gmail.com>
2023-01-06 09:45:16 +0530

    Create Package and CEB Database

--- src/main/java/com/example/ceb/CebApplication.java
+++ src/main/java/com/example/ceb/CebApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CebApplication.class, args);
+    }
 
 }
