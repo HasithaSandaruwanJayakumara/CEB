659f956e85ca88ff2d61b10a1f010614e210af04
HasithaSandaruwanJayakumara <texthasitha@gmail.com>
2023-01-06 09:35:24 +0530

    Create Project and add dependencies

---
+++ src/main/java/com/example/ceb/CebApplication.java
@@ -0,0 +1,13 @@
+package com.example.ceb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CebApplication.class, args);
+	}
+
+}
